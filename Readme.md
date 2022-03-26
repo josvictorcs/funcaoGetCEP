ee5b11a6f97583dba28670ed3d5581ff09cae2ca
Victor Cassiano <victorcassiano7@gmail.com>
2022-03-26 11:25:24 -0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+﻿# Função para coletar o CEP de uma String
+<center> Essa função recebe uma String como parâmetro e retorna o Cep ou -1 caso não exista. :mailbox: </center>
+
---
+++ extrator_cep.py
@@ -0,0 +1,10 @@
+import re
+def extrair_cep(endereco):
+    molde = re.compile('[0-9]{5}[-]?[0-9]{3}')
+    busca = molde.search(endereco)
+    if busca:
+        cep = busca.group()
+        return cep
+    else:
+        return -1
+
